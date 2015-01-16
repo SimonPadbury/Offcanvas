486c00532acbcad23482eb9a4e03c4c69e9919fb
SimonPadbury <simon_padbury@yahoo.co.uk>
2015-01-16 15:08:56 +0000

    Create offcanvas.js

---
+++ js/offcanvas.js
@@ -0,0 +1,32 @@
+(function ($) {
+	
+	"use strict";
+  
+	$(document).ready(function () {
+
+		// Off-canvas
+			
+		function off_canvas_open() {
+			$('body').addClass('offcanvas-open');
+		}
+		function off_canvas_close() {
+			$('body').removeClass('offcanvas-open');
+			$('.offcanvas-sidebar').delay(400).animate({scrollTop : 0}, 50);
+		}
+		$('.offcanvas-navicon').click(function () {
+			if (!$('body').hasClass('offcanvas-open')) {
+				off_canvas_open();
+			} else {
+				off_canvas_close();
+			}
+		});
+		$('.offcanvas-overlay').click(function () {
+			off_canvas_close();
+		});
+		$(window).resize(function () {
+			off_canvas_close();
+		});
+		
+	});
+
+}(jQuery));
